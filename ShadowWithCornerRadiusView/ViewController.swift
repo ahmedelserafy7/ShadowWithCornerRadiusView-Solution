ee708232f767bd0359d74b64b15b51f132195899
ahmedelserafy7 <ahmed.samer43@yahoo.com>
2020-01-30 22:38:48 +0200

    Initial Commit

---
+++ ShadowWithCornerRadiusView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShadowWithCornerRadiusView
+//
+//  Created by Elser_10 on 1/30/20.
+//  Copyright © 2020 Elser_10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ShadowWithCornerRadiusViewTests/ShadowWithCornerRadiusViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  ShadowWithCornerRadiusViewTests.swift
+//  ShadowWithCornerRadiusViewTests
+//
+//  Created by Elser_10 on 1/30/20.
+//  Copyright © 2020 Elser_10. All rights reserved.
+//
+
+import XCTest
+@testable import ShadowWithCornerRadiusView
+
+class ShadowWithCornerRadiusViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
