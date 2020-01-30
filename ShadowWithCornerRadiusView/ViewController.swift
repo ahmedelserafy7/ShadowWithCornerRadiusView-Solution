8aeb5f1bc37398ef68709a6bc5e4a6611720c85a
ahmedelserafy7 <ahmed.samer43@yahoo.com>
2020-01-30 23:48:43 +0200

    add commit

--- ShadowWithCornerRadiusView/ViewController.swift
+++ ShadowWithCornerRadiusView/ViewController.swift
@@ -9,17 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let subContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .white
+        
+        setupShadow()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func setupShadow() {
+        
+        view.addSubview(containerView)
+        containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        containerView.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        containerView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        containerView.backgroundColor = .clear
+        containerView.layer.shadowColor = UIColor.lightGray.cgColor
+        // or if you'd like to add shadow color in black, it's what you prefer
+        //containerView.layer.shadowColor = UIColor.black.cgColor
+        containerView.layer.shadowOffset = .zero
+        containerView.layer.shadowOpacity = 0.3
+        // shadow radius is optional, you can add or comment it
+        containerView.layer.shadowRadius = 8
+        containerView.layer.masksToBounds = false
+        
+        containerView.addSubview(subContainerView)
+        subContainerView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor).isActive = true
+        subContainerView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        subContainerView.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        subContainerView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        subContainerView.backgroundColor = .white
+        subContainerView.layer.cornerRadius = 8
+        subContainerView.layer.masksToBounds = true
+        
     }
-
-
+    
 }
-
